85714a58646c563c1459c839cf65a79d2410fe1c
Anupreet1213 <anupreet.2003@gmail.com>
2023-04-06 16:18:09 +0530

    Woeking on Projects section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,6 +4,43 @@ const Projects = () => {
   return (
     <section id="projects">
       <p class="projectsHeading">Projects</p>
+      <div className="sc-gZMcBi jwbuPS">
+        <div
+          role="gridcell"
+          id="cardHover"
+          tabindex="0"
+          class="sc-jTzLTM dFqYWZ"
+        >
+          <div class="content__slate">
+            <h3>CEmission</h3>
+            <p>Calculate your carbon footprints</p>
+            <p class="d-flex flex-wrap">
+              <span class="d-block mb-1">React</span>
+              <span class="d-block mb-1">CSS</span>
+              <span class="d-block mb-1">ML</span>
+              <span class="d-block mb-1">MUI</span>
+            </p>
+          </div>
+        </div>
+
+        <div
+          role="gridcell"
+          id="cardHover"
+          tabindex="0"
+          class="sc-jTzLTM dFqYWZ"
+        >
+          <div class="content__slate">
+            <h3>CEmission</h3>
+            <p>Calculate your carbon footprints</p>
+            <p class="d-flex flex-wrap">
+              <span class="d-block mb-1">React</span>
+              <span class="d-block mb-1">CSS</span>
+              <span class="d-block mb-1">ML</span>
+              <span class="d-block mb-1">MUI</span>
+            </p>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
