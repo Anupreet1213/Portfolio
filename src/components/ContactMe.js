7895dbba854a2160ea8941b00d14d0f0af1e624a
AmanApT <amanasstudent@gmail.com>
2023-04-07 15:14:41 +0530

    Contact Me Section

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,13 +1,42 @@
-import React from 'react'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTwitter } from "@fortawesome/free-solid-svg-icons";
 
 const ContactMe = () => {
   return (
-    <div className='contactMe'>
-        <p className='projectsHeading'>
-            Contact Me
+    <div className="contactMe">
+      <div className="contactMeUpDiv">
+        <p className="startAProject">Start a project</p>
+        <p className="startAProjectDescription">
+          Interested in working together? We should queue up a time to chat.
         </p>
-    </div>
-  )
-}
+        <div className="letsDoThisButton"> Let's Do This</div>
+      </div>
 
-export default ContactMe
+      <div className="contactMeDescription">
+        Living, learning, & leveling up one day at a time.
+      </div>
+
+      <div className="socialLinks">
+        <div className="socialLinkDiv">
+          <i class="fa-brands fa-twitter links"></i>
+        </div>
+        <div className="socialLinkDiv">
+          <i class="fa-brands fa-linkedin links"></i>
+        </div>
+        <div className="socialLinkDiv">
+          <i class="fa-brands fa-github links"></i>
+        </div>
+        <div className="socialLinkDiv">
+          <i class="fa-solid fa-envelope links"></i>
+        </div>
+      </div>
+
+      <div className="handcraftedBy">
+        <p>Handcrafted by me © Aman Pathak</p>
+      </div>
+    </div>
+  );
+};
+
+export default ContactMe;
